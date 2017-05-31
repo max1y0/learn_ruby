836c5ffd38d1be56f13efe6e8897d04cefe1015e
Maxi Chaves <max1y0@live.com.ar>
2017-05-31 11:44:39 -0300

    Solved 02 0.7 percent

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add (n,m)
+	n+m
+end
+
+def subtract (n,m)
+	n-m
+end
+
+def sum (a)
+	acum = 0
+	a.each do |i|
+		acum = acum +i
+	end
+	return acum
+end
+
+def mult (a)
+	acum = 1
+	a.each do |i|
+		acum = acum *i
+	end
+	return acum
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,10 +79,13 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do 
+    expect(mult([7,11])).to eq(77)   
+  end
 
-  it "multiplies several numbers"
-  
+  it "multiplies several numbers" do
+    expect(mult([1,3,5,7,9])).to eq(945)
+  end
 end
 
 describe "#power" do
