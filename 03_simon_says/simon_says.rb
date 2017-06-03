c2643223658e497302427beb5a1f31ec58acd5df
Maxi Chaves <max1y0@live.com.ar>
2017-06-03 09:44:13 -0300

    Solved 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
 #write your code here
+def echo(m)
+	return m
+end
+
+def shout(m)
+	return m.upcase	
+end	
+
+def repeat(m, n = 2)
+	([m] * n).join(" ")
+end
+
+def start_of_word (m,n)
+	return m[0...n]
+end
+
+def first_word (m)
+	return m.split(" ").first
+end
+
+def titleize (m)
+	words = m.split.map do |w|
+		if w == "and" || w == "the" || w == "over"
+			w
+		else
+			w.capitalize
+		end
+	end
+	words.first.capitalize!
+	words.join(" ")
+end
