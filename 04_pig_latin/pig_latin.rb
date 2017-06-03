7c65e6b1520640b7ab3d8522bba8c8587a02ef3e
Maxi Chaves <max1y0@live.com.ar>
2017-06-03 10:31:33 -0300

    Solved 04 0.8 percent

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,23 @@
 #write your code here
+def translateWord (m)
+	firstc = m.chars.first
+	case m
+	when /^[aeiouy]/ 					#regular expression for leading vowels
+		m + "ay"
+	when /^(qu+)(.*)/ 					#regular expression for leading qu. $1= qu $2 = rest
+		$2+$1+"ay"	
+	when /^([^aeiouy]+)(.*)/			#regular expression for leading consonants (not vowels) $1= complement of aeiou(consonant). $2 = rest
+		$2+$1+"ay"
+	else
+		m
+	end
+end
+
+
+def translate (m)
+	res = []
+	m.split.each do |w|
+		res<<translateWord(w)
+	end
+	return res.join(" ")
+end
